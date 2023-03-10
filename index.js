3e36e952c8ba85520a1f370c7942182e702be02d
Job koech <jobkoech610@gmail.com>
2023-03-10 17:08:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+//remove id main.
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+//create h1 element with newHeader variable.
+const newHeader = document.createElement('h1');
+
+//set id to victory.
+newHeader.setAttribute('id','victory');
+
+//add text to newheader variable
+newHeader.textContent = 'JOB is the champion';
+
+
+
+
